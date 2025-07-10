5b7ade699546b71267e34b197c08d7a34e59f224
MatthewJohnKing <137497118+MatthewJohnKing@users.noreply.github.com>
2025-07-10 13:30:52 +0000

    update with the program

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,9 +3,13 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+#include <string>
 int main(){
-    
+    std::string name;
+    std::cout << "Enter your name: "<< std::flush;
+    std::cin >> name;
+    //only works for single words
+    std::cout << "Nice to meet you " << name << "!" << std::endl;
     std::cout << std::endl << std::endl;
     return 0;
 }
